986ddc24299c0d225e4aed9e159e83a7bdd3a4d4
Mike Ackerman <mikeackerman@gmail.com>
2013-11-25 17:27:41 -0700

    Adds seed Update.

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,7 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+if Update.count == 0
+  Update.create(:status => "taken")
+end
